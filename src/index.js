30fdb59187a319f60f9b6cb1518484bd12df4349
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-06-10 07:54:42 +0300

    add dowload several words

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import fs from 'fs';
 import axios from 'axios';
+import path from 'path';
 
 const resources = [
   'https://images.puzzle-english.com/words/campbridge_UK/',
-  // 'https://images.puzzle-english.com/words/macmillan_UK/',
-  // 'https://images.puzzle-english.com/words/howjsay_UK/',
+  'https://images.puzzle-english.com/words/macmillan_UK/',
+  'https://images.puzzle-english.com/words/howjsay_UK/',
 ];
 
-async function getFiles(url) {
+async function getFile(url) {
   return axios({
     method: 'get',
     url,
@@ -15,20 +16,35 @@ async function getFiles(url) {
   });
 }
 
-const run = () => {
-  const newWords = ['someone'];
+async function getData(links) {
+  const promises = links.map((link) => getFile(link));
+
+  return Promise.all(promises);
+}
+
+async function downloadFiles(links) {
+  const stat = {};
+  const files = await getData(links);
+
+  files.forEach((response) => {
+    if (response.status === 200) {
+      const url = new URL(response.config.url);
+      const fileName = path.basename(url.pathname, '.mp3');
+      stat[fileName] = (stat[fileName] !== undefined) ? stat[fileName] + 1 : 1;
+
+      response.data.pipe(fs.createWriteStream(`./data/${fileName}${stat[fileName]}.mp3`));
+    }
+  });
+}
+
+async function run() {
+  const newWords = ['someone', 'bowl'];
 
   const links = newWords.map((word) => (
     resources.map((resource) => `${resource}${word}.mp3`)
   ));
 
-  const result = getFiles(links.flat()[0]);
-  result
-    .then((response) => {
-      if (response.status === 200) {
-        response.data.pipe(fs.createWriteStream('./data/someone.mp3'));
-      }
-    });
-};
+  downloadFiles(links.flat());
+}
 
 export default run;
